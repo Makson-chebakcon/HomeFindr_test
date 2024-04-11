b75c825b1a44d731f674174244793b121006ff70
Makson-chebakcon <makc15655@gmail.com>
2024-04-12 02:14:05 +0700

    Сделал меню навигации, настроил перемещение по фрагментам, так же сделал страницу списка чатов и шаблон карты чата

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -64,6 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.mediarouter:mediarouter:1.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -71,4 +71,5 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
--- app/src/main/java/com/example/thi_comand_work/presentation/singIn/MainSingInActivity.kt
+++ app/src/main/java/com/example/thi_comand_work/presentation/singIn/MainSingInActivity.kt
@@ -2,23 +2,68 @@ package com.example.thi_comand_work.presentation.singIn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.example.thi_comand_work.R
 import com.example.thi_comand_work.databinding.ActivityMainBinding
-import com.example.thi_comand_work.presentation.auth.ChoosAuthRegistration
+import com.example.thi_comand_work.databinding.ActivityMainSingInBinding
 
 class MainSingInActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainSingInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainSingInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val tb = binding.tbMainScreen
+        setSupportActionBar(tb)
         openFragment(CardsListFragment.newInstance())
+        binding.tbMainScreen.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId){
+                R.id.menuIcon ->{
+                    binding.draver.openDrawer(GravityCompat.START)
+                    true
+                }
+                else -> false
+            }
+        }
+        binding.navigationMenu.setNavigationItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.menuProfile ->{
+                    openFragment(ProfileFragment.newInstance())
+                    binding.draver.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.menuCardList ->{
+                    openFragment(CardsListFragment.newInstance())
+                    binding.draver.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.menuSetings ->{
+                    //todo добавить страницу настроек
+                    binding.draver.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.menuChat ->{
+                    openFragment(ChatListFragment.newInstance())
+                    binding.draver.closeDrawer(GravityCompat.START)
+                    true
+                }
+
+                else -> false
+            }
+
+        }
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.tool_bar, menu)
+        return true
+    }
+
     private fun openFragment(fragment: Fragment){
         supportFragmentManager.beginTransaction()
-            .replace(R.id.mainFragmentContainer, fragment).commit()
-
-
+            .replace(R.id.fragmentContainerView, fragment).commit()
     }
+
+
 }
